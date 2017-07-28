b3fd00f46099ba3698b8878b3cb08a413e5579d5
nickells <ellsworth.nick@gmail.com>
2017-07-27 23:34:14 -0400

    new website

--- script.js
+++
@@ -1,3 +0,0 @@
-const message = 'Hello and welcome to Nick Ellsworth\'s website.'
-
-typer(message, document.getElementById('main')).setBaseSpeed(80).setPercentError(10).go()
---
+++ src/scripts/index.js
@@ -0,0 +1,3 @@
+// require index.html so livereload will watch it
+const index = require('../../index.html') // eslint-disable-line no-unused-vars
+
