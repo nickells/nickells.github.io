d3d054137b27d765313b7e724ed86fc5d8ced991
nickells <ellsworth.nick@gmail.com>
2017-09-18 23:32:04 -0400

    clock works

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,7 @@
 // require index.html so livereload will watch it
 const index = require('../../index.html') // eslint-disable-line no-unused-vars
+const clock = require('./clock')
 
+console.log(clock)
+
+clock.start()
