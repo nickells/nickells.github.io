c619521b204dad0a5cc38dea9a5da34626d2fed3
Nick Ellsworth <nellsworth@squarespace.com>
2018-11-18 23:23:20 -0500

    added unfriendly gate

---
+++ src/scripts/gate.js
@@ -0,0 +1,35 @@
+const links = Array.from(document.querySelectorAll("a[data-unfriendly]"))
+
+const hijack = (evt) => {
+  const oldDom = evt.target.innerHTML
+  evt.preventDefault()
+  evt.stopPropagation()
+  const dom = `
+    note: I didn't really build this with your touch device in mind. It might not work.
+
+    Continue?
+    <div>
+      <button class="no">No</button><button class="yes">Yes</button>
+    </div>
+  `
+  const $warning = document.createElement('div')
+  $warning.classList.add('gate')
+  $warning.innerHTML = dom
+  document.body.appendChild($warning)
+  $warning.getElementsByClassName('no')[0].addEventListener('touchend', () => {
+    setTimeout(() => {
+
+      document.body.removeChild($warning)
+    }, 500)
+  })
+  $warning.getElementsByClassName('yes')[0].addEventListener('touchend', () => {
+    window.location.href = evt.target.href
+  })
+}
+
+const hijackLinks = () => {
+  console.log('doing')
+  links.forEach(elem => elem.addEventListener('click', hijack))
+}
+
+module.exports = hijackLinks
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,8 @@
 // require index.html so livereload will watch it
 const index = require('../../index.html') // eslint-disable-line no-unused-vars
 const clock = require('./clock')
+const gate = require('./gate')
 
 
 clock.start()
+if ('ontouchstart' in window) gate()
